e632080520108b5944bd4b1de4b3f944c474d0d0
meenuga-meghana <marthadass94@gmail.com>
2025-07-18 16:20:36 +0530

    Delete poject files/AutoAssignRequest.js

--- poject files/AutoAssignRequest.js
+++
@@ -1,7 +0,0 @@
-
-// AutoAssignRequest.js
-(function executeRule(current, gForm, gUser, gSNC) {
-    if (!current.assigned_to) {
-        current.assigned_to = 'IT Support Group';
-    }
-})(current, gForm, gUser, gSNC);
