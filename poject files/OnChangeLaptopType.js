4a4fe71874fa72ee3e58c8cd2bde6f82134f18b0
meenuga-meghana <marthadass94@gmail.com>
2025-07-18 16:20:51 +0530

    Delete poject files/OnChangeLaptopType.js

--- poject files/OnChangeLaptopType.js
+++
@@ -1,8 +0,0 @@
-
-// OnChangeLaptopType.js
-function onChange(control, oldValue, newValue, isLoading) {
-    if (isLoading || newValue == '') {
-        return;
-    }
-    g_form.setDisplay('justification', true);
-}
